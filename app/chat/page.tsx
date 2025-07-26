4ceba58e281fc331eea4bb67b1d3e67fe37d3719
manishkharthik <manishkharthik0630@gmail.com>
2025-07-26 11:40:49 +0800

    Fix: move useSearchParams into client component

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import dynamic from "next/dynamic";
-import { Suspense } from 'react';
 
 const ChatPageClient = dynamic(() => import("./ChatPageClient"), { ssr: false });
 
 export default function ChatPage() {
   return (
-    <Suspense fallback={<div>Loading chat...</div>}>
-      <ChatPageClient />
-    </Suspense>
+    <ChatPageClient />
   );
 }
